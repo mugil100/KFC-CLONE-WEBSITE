dbb956b0e617999ae979741977f2b019951172de
mugil100 <expectopetronum002@gmail.com>
2024-11-27 23:02:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+ swiper = new Swiper('.wrapper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween:30,
+  
+    // pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+
+    }
+  });
